7b067608cc1c48a516116e1dc7e0fbb5d99a56b5
Lokitosi10 <50706643+Lokitosi10@users.noreply.github.com>
2020-10-20 11:42:04 -0500

    se modifico peso

--- bocchi.js
+++ bocchi.js
@@ -22,6 +22,18 @@ client.on('message', (message) => {
     }
 
 });
+
+client.on('message', (message) => {
+    if (message.content.startsWith(prefix + '7w7')) {
+        message.channel.send('La recomendacion del dia');
+        function aleatorio(a,b) {
+            return Math.round(Math.random()*(b-a)+parseInt(a));
+            }
+        a = aleatorio(100000,500000);
+        message.channel.send("https://nhentai.to/g/"+a);
+    }
+
+});
 // trabajo
 
 client.on('message', (message) => {
