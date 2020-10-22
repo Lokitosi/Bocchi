927fbad7a1f1786c529e349f488f6102e79a99cb
Lokitosi10 <50706643+Lokitosi10@users.noreply.github.com>
2020-10-22 07:01:01 -0500

    funcion doujinshi funcional, buscar manera de optimizar

--- Comandos/doujin.js
+++ Comandos/doujin.js
@@ -1,8 +1,23 @@
 module.exports = (client, message, args) => {
-    message.channel.send('La recomendacion del dia');
-        function aleatorio(a,b) {
-            return Math.round(Math.random()*(b-a)+parseInt(a));
+    const cheerio = require('cheerio');
+    const request = require('request');
+    message.channel.send('La recomendacion del dia <a:loading:768805475025551450>');
+    function aleatorio(a, b) {
+        return Math.round(Math.random() * (b - a) + parseInt(a));
+    }
+
+    a = aleatorio(100000, 500000);
+    intento(a);
+    function intento(num) {
+        request('https://nhentai.to/g/' + num, (err, res, body) => {
+
+            if (!err && res.statusCode == 200) {
+                message.channel.send('https://nhentai.to/g/' + a);
+            } else {
+                a = aleatorio(100000, 500000);
+                intento(a);
             }
-        a = aleatorio(100000,500000);
-        message.channel.send("https://nhentai.to/g/"+a);
+        }
+        )
+    }
 }
