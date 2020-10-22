720cbf3bb573d0a49513318789117fa7597395f0
Lokitosi10 <50706643+Lokitosi10@users.noreply.github.com>
2020-10-22 08:29:39 -0500

    funcion doujinshi arreglada, agregada busqueda en MAL

--- Comandos/help.js
+++ Comandos/help.js
@@ -5,10 +5,11 @@ message.channel.send('Lista de comandos');
 
 const embed = new Discord.MessageEmbed()
     .setAuthor(client.user.username, client.user.avatarURL())
-    .addField('+doujin', 'Te recomienda un doujinshi aleatorio (beta)')
+    .addField('+doujin', 'Te recomienda un doujinshi aleatorio')
     .addField('+help', 'muestra todos los comandos')
     .addField('+oferta', 'Muestra ofertas laborales')
     .addField('+saludo', 'Bocchi te saluda')
+    .addField('+mal', 'Te recomienda un anime aleatorio en My Anime List')
     .addField('+trabajo', 'busca ofertas laborales por las palabras dadas despues del comando')
     .setFooter("Version 0.5--Bocchi super fuerte de servicio", client.user.avatarURL())
     .setColor(16580705)
--- Comandos/mal.js
+++ Comandos/mal.js
@@ -1,3 +1,5 @@
+const { MessageManager } = require('discord.js');
+
 module.exports = (client, message, args) => {
     const cheerio = require('cheerio');
     const request = require('request');
@@ -7,16 +9,16 @@ module.exports = (client, message, args) => {
         return Math.round(Math.random() * (b - a) + parseInt(a));
     }
 
-    a = aleatorio(100000, 500000);
+    a = aleatorio(1,40750);
     intento(a);
     function intento(num) {
-        request('https://nhentai.to/g/' + num, (err, res, body) => {
+        request('https://myanimelist.net/anime/'+ num, (err, res, body) => {
 
             if (!err && res.statusCode == 200) {
                 message.channel.bulkDelete(1);
-                message.channel.send('https://nhentai.to/g/' + a);
+                message.channel.send('https://myanimelist.net/anime/' + num);
             } else {
-                a = aleatorio(100000, 500000);
+                a = aleatorio(1,40750);
                 intento(a);
             }
         }
